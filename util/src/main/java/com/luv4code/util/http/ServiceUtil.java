8237ac9327114c99e2a9be0c60cb54d3eed535aa
spring-dev-master <madhav.p.akv@outlook.com>
2022-05-17 21:22:17 +0530

    updating util service

---
+++ util/src/main/java/com/luv4code/util/http/ServiceUtil.java
@@ -0,0 +1,42 @@
+package com.luv4code.util.http;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class ServiceUtil {
+    private final String port;
+
+    private String serviceAddress = null;
+
+    @Autowired
+    public ServiceUtil(@Value("${server.port}") String port) {
+
+        this.port = port;
+    }
+
+    public String getServiceAddress() {
+        if (serviceAddress == null) {
+            serviceAddress = findMyHostname() + "/" + findMyIpAddress() + ":" + port;
+        }
+        return serviceAddress;
+    }
+
+    private String findMyHostname() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            return "unknown host name";
+        }
+    }
+
+    private String findMyIpAddress() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            return "unknown IP address";
+        }
+    }
+}
