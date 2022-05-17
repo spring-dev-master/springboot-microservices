65f51fd904fd572d50f236f6a0606e603a6693b0
spring-dev-master <madhav.p.akv@outlook.com>
2022-05-17 19:49:29 +0530

    creating initial project skeleton

---
+++ src/test/java/com/luv4code/springboot/microservices/SpringbootMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv4code.springboot.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
